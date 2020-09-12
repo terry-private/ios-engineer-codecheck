4617588700e9d3aef32940a06a00626db2c6c585
teruhito <ataeteruhito@gmail.com>
2020-09-12 23:57:16 +0900

    #2 命名修正 API Design Guidelines ・Avoid abbreviations. に則って略語を訂正 ・Follow case conventions. に則って大文字小文字の規則を統一 ・その他役割と名前が結びつかない部分を修正

---
+++ iOSEngineerCodeCheck/RepositoryViewController.swift
@@ -0,0 +1,56 @@
+//
+//  RepositoryViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 史 翔新 on 2020/04/21.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var watchersLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var issuesLabel: UILabel!
+    
+    var repositoriesTableViewController: RepositoriesTableViewController!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let repo = repositoriesTableViewController.repositories[repositoriesTableViewController.index]
+        
+        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
+        starsLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        watchersLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+        getImage()
+        
+    }
+    
+    func getImage(){
+        
+        let repo = repositoriesTableViewController.repositories[repositoriesTableViewController.index]
+        
+        titleLabel.text = repo["full_name"] as? String
+        
+        if let owner = repo["owner"] as? [String: Any] {
+            if let imgURL = owner["avatar_url"] as? String {
+                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+                    let img = UIImage(data: data!)!
+                    DispatchQueue.main.async {
+                        self.imageView.image = img
+                    }
+                }.resume()
+            }
+        }
+        
+    }
+    
+}
