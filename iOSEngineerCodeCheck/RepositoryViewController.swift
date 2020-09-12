279072f2e2ef1a3c36ddfca092c95160ae904950
teruhito <ataeteruhito@gmail.com>
2020-09-13 00:13:47 +0900

    #3 #2 RepositoryViewController > getImage 内の修正 ・強制アンラップの廃止 ・エラーでreturn ・無駄なネストを解決

--- iOSEngineerCodeCheck/RepositoryViewController.swift
+++ iOSEngineerCodeCheck/RepositoryViewController.swift
@@ -31,26 +31,28 @@ class RepositoryViewController: UIViewController {
         forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
         issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
-        
     }
     
-    func getImage(){
-        
+    func getImage() {
         let repo = repositoriesTableViewController.repositories[repositoriesTableViewController.index]
-        
         titleLabel.text = repo["full_name"] as? String
         
-        if let owner = repo["owner"] as? [String: Any] {
-            if let imgURL = owner["avatar_url"] as? String {
-                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
-                    let img = UIImage(data: data!)!
-                    DispatchQueue.main.async {
-                        self.imageView.image = img
-                    }
-                }.resume()
-            }
+        guard let owner = repo["owner"] as? [String: Any] else { return }
+        guard let urlString = owner["avatar_url"] as? String else { return }
+        // URLの強制アンラップを廃止し事前にエラーとしてreturn
+        guard let imgURL = URL(string: urlString) else {
+            print("urlエラー")
+            return
         }
-        
+        URLSession.shared.dataTask(with: imgURL) { (data, res, err) in
+            if let err = err {
+                print("err: \(err)")
+                return
+            }
+            guard let data = data, let img = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.imageView.image = img
+            }
+        }.resume()
     }
-    
 }
